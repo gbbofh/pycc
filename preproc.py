00c37259108edbb1777d634224c5da443c5bd3ec
Garrett Bates <garrett.bates@student.nmt.edu>
2019-11-04 17:30:29 -0700

    Fixed some studidity in the preprocessor prototype

--- preproc.py
+++ preproc.py
@@ -12,20 +12,16 @@ class Preprocessor():
     def process(self, lines):
         new_lines = []
         for line in lines:
-            re.sub('//.*$', '', line)
-            m = re.search('(#define)', line)
+            re.sub('//.+$', '', line)
+            m = re.search('#define', line)
             if m:
-                ident = re.search('[A-Za-z_][A-Za-z_0-9]*', line[m.end() : ])
-                val = re.search('.*', line[ident.end() + m.end() : ])
-                if val:
-                    line = line[0 : m.start()] + line[m.end() + val.end() + ident.end():]
-                    self.symbols[ident.group()] = val.group()
-                else:
-                    line = line[0 : m.start()] + line[m.end() + ident.end():]
-                    self.symbols[ident.group()] = ''
+                ident = re.search('[A-Za-z_][A-Za-z0-9_]*', line[m.end() : ])
+                val = line[m.end() + ident.end() : ]
+                self.symbols[ident.group().strip()] = val.strip()
+                line = line[0 : m.start()]
             for i in self.symbols:
                 m = re.search(i, line)
                 if m:
-                    re.sub(m.re, self.symbols[i], line) 
+                    line = re.sub(m.re, self.symbols[i], line) 
             new_lines.append(line)
         return new_lines
